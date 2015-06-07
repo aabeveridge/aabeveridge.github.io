66d7a2f3dbf018562610ab1083fc19cfe3b95333
Aaron Beveridge <aaronbev79@gmail.com>
2015-06-07 13:51:25 -0400

    images, home page, and first content added

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 pandoc head.md -s -c pandoc.css -o head.html
-pandoc home.md -s -c pandoc.css -o index.html --include-in-header=head.html
-pandoc cv.md -s -c pandoc.css -o cv.html --include-in-header=head.html
-pandoc projects.md -s -c pandoc.css -o projects.html --include-in-header=head.html
-pandoc teaching.md -s -c pandoc.css -o teaching.html --include-in-header=head.html
+pandoc home.md -s -c pandoc.css -o index.html --smart --include-in-header=head.html
+pandoc cv.md -s -c pandoc.css -o cv.html --smart --include-in-header=head.html
+#pandoc projects.md -s -c pandoc.css -o projects.html --smart --include-in-header=head.html
+#pandoc teaching.md -s -c pandoc.css -o teaching.html --smart --include-in-header=head.html
 #git add --all
 #git commit -m "site update"
 #git push
