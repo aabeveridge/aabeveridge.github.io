07850358ed92e09cad8f0328fe3bff83b3340342
Aaron Beveridge <aaronbev79@gmail.com>
2015-06-08 18:35:02 -0400

    add page titles for tabs

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 pandoc head.md -s -c pandoc.css -o head.html
-pandoc home.md -s -c pandoc.css -o index.html --smart --include-in-header=head.html
-pandoc cv.md -s -c pandoc.css -o cv.html --smart --include-in-header=head.html
-#pandoc projects.md -s -c pandoc.css -o projects.html --smart --include-in-header=head.html
-#pandoc teaching.md -s -c pandoc.css -o teaching.html --smart --include-in-header=head.html
+pandoc home.md -s -c pandoc.css -o index.html --smart --include-in-header=head.html --title-prefix="Aaron Beveridge"
+pandoc cv.md -s -c pandoc.css -o cv.html --smart --include-in-header=head.html --title-prefix="CV"
+pandoc projects.md -s -c pandoc.css -o projects.html --smart --include-in-header=head.html --title-prefix="Projects" 
+pandoc teaching.md -s -c pandoc.css -o teaching.html --smart --include-in-header=head.html --title-prefix="Teaching"
 #git add --all
 #git commit -m "site update"
 #git push
--- home.md
+++ home.md
@@ -1,5 +1,6 @@
-<img src="./images/headshot1_medium.jpg" align="left" style="display:inline;margin:9px 12px 6px 0px;"/>
+<img src="./images/headshot1_medium.jpg" align="left" style="display:inline;margin:0px 12px 6px 0px;"/>
 
 My research investigates the intersection of writing studies and data science research paradigms---focusing on the importance of programming and software development as they motivate the ongoing expansion of available methods in media ecology and the posthumanities. Grounded primarily in circulation studies, my research interests also include professional and technical writing, the rhetoric of science, and maker culture.  
-**writing studies | data science | media ecology**
+
+####**writing studies | data science | media ecology**
 
